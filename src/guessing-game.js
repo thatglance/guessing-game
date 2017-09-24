f474de9cb0f660926a8da8f0f1f3481dc4eaafd4
thatglance <11sashamak@gmail.com>
2017-09-24 16:07:24 +0300

    first commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.max + this.min)/2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
